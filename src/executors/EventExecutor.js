934503813e49a87fdd30b7e3f8dc09bc8bab7084
Joshua Jung <josh@jungdigital.com>
2017-02-09 08:25:19 -0600

    Ensure that if a triggered event times out that the EventExecutor that triggered it clears its own timeout.

--- src/executors/EventExecutor.js
+++ src/executors/EventExecutor.js
@@ -93,6 +93,9 @@ class EventExecutor extends ExecutorAbstract {
     // Alright, this failure is a timeout on our dispatched event, so the other handling thread will have already dealt with it. Don't display
     // the error again.
     if (this.dispatchedRingaEvent._threadTimedOut) {
+      // Lets clear our own timeout and just neither be done nor fail.
+      this.endTimeoutCheck();
+
       return;
     }
 
