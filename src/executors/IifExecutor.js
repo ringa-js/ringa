2690f847d052e843226ad040038a25683ced4048
Thomas Y <corragon@gmail.com>
2017-02-10 01:03:53 -0600

    Add comments on IifExecutor

--- src/executors/IifExecutor.js
+++ src/executors/IifExecutor.js
@@ -4,6 +4,17 @@ import {getArgNames} from '../util/function';
 import {buildArgumentsFromRingaEvent} from '../util/executors';
 
 class IifExecutor extends ExecutorAbstract {
+  //-----------------------------------
+  // Constructor
+  //-----------------------------------
+  /**
+   * Constructor for a new IifExecutor.
+   *
+   * @param thread The parent thread that owns this command.
+   * @param condition The result of this function determines which executor to run
+   * @param trueExecutor  The executor run if condition returns a truthy value
+   * @param falseExecutor The executor run if condition returns a falsy value
+   */
   constructor(thread, { condition, trueExecutor, falseExecutor }) {
     super(thread);
 
@@ -12,6 +23,17 @@ class IifExecutor extends ExecutorAbstract {
     this.falseExecutor = falseExecutor;
   }
 
+  //-----------------------------------
+  // Methods
+  //-----------------------------------
+
+  /**
+   * Internal execution method called by CommandThread only.
+   *
+   * @param doneHandler The handler to call when done() is called.
+   * @param failHandler The handler to call when fail() is called;
+   * @private
+   */
   _execute(doneHandler, failHandler) {
     super._execute(doneHandler, failHandler);
     
